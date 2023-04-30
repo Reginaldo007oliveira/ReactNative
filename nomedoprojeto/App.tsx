9b5f2f578bbb6d799115a7031651abf32fe53d24
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-04-30 20:19:58 -0300

    Organizando o Projeto

--- nomedoprojeto/App.tsx
+++ nomedoprojeto/App.tsx
@@ -1,35 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/screens/Home";
 export default function App() {
   return (
     
-    <View style={styles.container}>
-    <Text style={styles.eventName}>
-      Nome do evento
-    </Text>
-
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
-    </View>
+    <Home />
 
 )
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24,
-    display:"flex",
-    alignItems:"center",
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 100,
-    fontWeight: 'bold',
-    marginTop: 48
-  },
-  eventDate: {
-    color: '#6B6B6B',
-    fontSize: 16
-  }
-})
---
+++ nomedoprojeto/src/screens/Home/style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24,
+      display:"flex",
+      alignItems:"center",
+    },
+    eventName: {
+      color: '#FDFCFE',
+      fontSize: 100,
+      fontWeight: 'bold',
+      marginTop: 48
+    },
+    eventDate: {
+      color: '#6B6B6B',
+      fontSize: 16
+    }
+  })
