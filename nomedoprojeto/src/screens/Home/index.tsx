543df656a6d5663f52ed0975eb24dba52e4e450b
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-01 15:35:12 -0300

    Adicionando usuários por meio do Use Staste

--- nomedoprojeto/src/screens/Home/index.tsx
+++ nomedoprojeto/src/screens/Home/index.tsx
@@ -1,16 +1,19 @@
 import {  Alert, FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
-
+import { useState } from "react";
 import { styles } from "./style";
 import { Participant } from "../../components/Participant";
 
 export  function Home() {
-
-  const participants = ['Rodrigo', 'Vini', 'Diego', 'Biro', 'Ana', 'Isa', 'Jack', 'Mayk', 'João',' Reginaldo'];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState('');
 
   function handleParticipantAdd() {
-    if (participants.includes("Rodrigo")) {
+    if (participants.includes(participantName)) {
       return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.");
     }
+    setParticipants(prevState => [...prevState, participantName]);
+    setParticipantName('');
+    console.log(participants)
   }
   function handleParticipantRemove(name: string){
     Alert.alert("Remover", `Remover o participante ${name}?`, [
@@ -38,6 +41,8 @@ export  function Home() {
         style={styles.input}
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
+        onChangeText={setParticipantName}
+          value={participantName}
        
       />
       <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
