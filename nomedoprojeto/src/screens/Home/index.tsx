7d0abbc71bf9e0437ff79de5da7a1b0e8c0490b0
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-01 07:37:14 -0300

    Component Participants

--- nomedoprojeto/src/screens/Home/index.tsx
+++ nomedoprojeto/src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./style";
+import { Participant } from "../../components/Participant";
 
 export  function Home() {
   function handleParticipantAdd() {
@@ -28,6 +29,9 @@ export  function Home() {
         </Text>
       </TouchableOpacity>
       </View>
+      <Participant />
+      <Participant />
+      <Participant />
     </View>
   )
 }
